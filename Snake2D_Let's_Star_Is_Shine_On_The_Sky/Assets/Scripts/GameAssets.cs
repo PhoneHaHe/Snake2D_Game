a2b6d58a2d77888711ea5a846938f94bce448393
Sarit Bas <basboss1818@gmail.com>
2021-06-25 21:42:29 +0700

    SetUp Scene & Create Snake Movement
    สร้างพื้นที่สี่เหลี่ยม และสร้างตัวหัวของงู กับการเคลื่อนไหวของงู

---
+++ Snake2D_Let's_Star_Is_Shine_On_The_Sky/Assets/Scripts/GameAssets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+
+    public static GameAssets gameAsset;
+
+    private void Awake() {
+        gameAsset = this;
+    }
+
+    public Sprite snakeHeadSprite;
+    public Sprite snakeBodySprite;
+
+    public Sprite appleSprite;
+    public Sprite grapeSprite;
+
+}
---
+++ Snake2D_Let's_Star_Is_Shine_On_The_Sky/Assets/Scripts/Snake.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2Int gridPosition;
+    private Vector2Int gridMoveDirection;
+    private float gridMoveTimer;
+    private float gridMoveTimerMax;
+    private void Awake()
+    {
+        gridPosition = new Vector2Int(1, 1);
+        gridMoveTimerMax = .5f;
+        gridMoveTimer = gridMoveTimerMax;
+        gridMoveDirection = new Vector2Int(1, 0);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        HandleInput();
+        HandleGridMovement();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (gridPosition.y != -1)
+            {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = +1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (gridPosition.y != +1)
+            {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = -1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (gridPosition.x != +1)
+            {
+                gridMoveDirection.x = -1;
+                gridMoveDirection.y = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (gridPosition.x != -1)
+            {
+                gridMoveDirection.x = +1;
+                gridMoveDirection.y = 0;
+            }
+        }
+    }
+
+    private void HandleGridMovement()
+    {
+        gridMoveTimer += Time.deltaTime;
+        if (gridMoveTimer >= gridMoveTimerMax)
+        {
+            gridPosition += gridMoveDirection;
+            gridMoveTimer -= gridMoveTimerMax;
+        }
+
+        transform.position = new Vector3(gridPosition.x, gridPosition.y);
+        transform.eulerAngles = new Vector3(0,0,GetAngleDirectionFromVector(gridMoveDirection) - 90);
+    }
+
+    private float GetAngleDirectionFromVector(Vector2Int other){
+        float n = Mathf.Atan2(other.y,other.x) * Mathf.Rad2Deg;
+        if(n < 0) n += 360;
+        return n;
+    }
+}
